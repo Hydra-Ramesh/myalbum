9d06e5e2a8c380f919f3a0389016ed5e2c120e4d
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2025-12-08 02:23:30 +0530

    Init the Project Structure:

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className="text-center">
+      {/*Make the following h1 and Button tag in Center*/}
+      <h1 className='text-blue-600'>Hey It is Your Application</h1>
+      <button className='mt-4 px-4 py-2 bg-green-500 text-white rounded'>Click Me</button>
+    </div>
+    </>
+  )
+}
+
+export default App
